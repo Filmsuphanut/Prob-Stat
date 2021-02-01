32a38672cab8c1d6089e079c148254dce161cf04
Filmsuphanut <77426667+Filmsuphanut@users.noreply.github.com>
2021-02-01 21:15:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 data = pd.read_csv('Data.csv')
 
-data_F_sort = data.sort_values(by=['Fuel_Cost_6000_Miles'])
+data_F_sort = data.sort_values(by=['CO_Emissions'])
 
 plt.figure(figsize=(6.8,4.2))
-plt.plot(data_F_sort['Fuel_Cost_6000_Miles'], data_F_sort['CO_Emissions'])
-plt.xlabel('Fuel_Cost_6000_Miles')
-plt.ylabel('CO_Emissions')
+plt.plot(data_F_sort['CO_Emissions'],data_F_sort['Fuel_Cost_6000_Miles'])
+plt.ylabel('Fuel_Cost_6000_Miles')
+plt.xlabel('CO_Emissions')
 plt.show()

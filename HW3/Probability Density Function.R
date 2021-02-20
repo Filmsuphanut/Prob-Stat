f4ebea406d80bbd481e5e26e437018973aa563b9
Filmsuphanut <nongfilm2987@gmail.com>
2021-02-21 00:46:24 +0700

    graph 80%

--- HW3/Probability Density Function.R
+++ HW3/Probability Density Function.R
@@ -1,5 +1,25 @@
 data <- read.csv("Data7.csv")
-data
+
+data.seq <- seq(1,709,length = 709)
+data.seq
+datadiv.seq <- data.seq/709
+datadiv.seq
+
+dataCO.sort <- sort(data$CO_Emissions)
+dataCO.med <- median(dataCO.sort)
+dataCO.sd <- sd(dataCO.sort)
+
+dataCO.dnorm <- dnorm(dataCO.sort,dataCO.med,dataCO.sd)
+plot(dataCO.sort,dataCO.dnorm,type = 'l', xlab="Probability Density",xlim=c(0,2100),ylim=c(0,0.0025))
+
+plot(dataCO.sort,datadiv.seq,type = 'l')
 
 
-pnorm(27.4, mean=50, sd=20)
+dataFuel.sort <- sort(data$Fuel_Cost_6000_Miles)
+dataFuel.med <- median(dataFuel.sort)
+dataFuel.sd <- sd(dataFuel.sort)
+
+dataFuel.dnorm <- dnorm(dataFuel.sort,dataFuel.med,dataFuel.sd)
+plot(dataFuel.sort,dataFuel.dnorm,type='l',xlim=c(0,2100),ylim=c(0,0.0025))
+
+plot(dataFuel.sort,datadiv.seq,type = 'l')

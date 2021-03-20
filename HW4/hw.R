2b3ee7859103da80ef45574cb699d98f60567126
suphanut wandee <77426667+Filmsuphanut@users.noreply.github.com>
2021-03-20 20:17:20 +0700

    มั่วหมดเลยไอสัส

--- HW4/hw.R
+++ HW4/hw.R
@@ -1,25 +1,44 @@
 data <- read.csv("Data.csv")
-data <- data$CO_Emissions
-data <- sort(data)
-data
-n <- length(data)
+
+dataCO.sort <- sort(data$CO_Emissions)
+dataCO.mean <- mean(dataCO.sort)
+dataCO.sd <- sd(dataCO.sort)
+n <- length(dataCO.sort)
 n
 
-data.med <- median(data)
-data.sd <- sd(data)
+dataCO.mean
+dataCO.sd
 
-data.med
-data.sd
-
-alpha <- (1-(95/100))
+alpha <- 0.1
 alpha
 
-data.z <- qnorm(p=alpha/2 , lower.tail=FALSE)
-data.z
+dataCO.z <- qnorm(p=alpha/2 , lower.tail=FALSE)
+dataCO.z
 
 
-lowerbound <- (data.med - ( data.z * data.sd/sqrt(n) ))
-upperbound <- (data.med + ( data.z * data.sd/sqrt(n) ))
+lowerbound <- (dataCO.mean - ( dataCO.z * dataCO.sd/sqrt(n) ))
+upperbound <- (dataCO.mean + ( dataCO.z * dataCO.sd/sqrt(n) ))
 
-lowerbound
-upperbound
+con.in <- c(lowerbound,upperbound)
+con.in
+
+dataCO.sort
+ran <- sample(dataCO.sort,500)
+ran.sort <- sort(ran)
+
+ran.mean <- mean(ran)
+
+ran.mean
+ran.sd <- sd(ran)
+
+
+dataCO.dnorm <- dnorm(dataCO.sort,dataCO.mean,dataCO.sd)
+plot(dataCO.sort,dataCO.dnorm,type = 'l',main ="CI 90% of Mean", ylab="Probability Density",xlab = "CO_Emissions (mg/km)",xlim=c(0,2000),ylim=c(0,0.0015))
+abline(v=upperbound,col = 'blue')
+abline(v=lowerbound,col = 'red')
+abline(v=dataCO.mean,col = 'green')
+
+
+
+#dataCO.dnorm <- dnorm(ran.sort,ran.mean,ran.sd)
+#plot(ran.sort,dataCO.dnorm,type = 'l',main ="Probability Density of CO Emissions", ylab="Probability Density",xlab = "CO_Emissions (mg/km)",xlim=c(0,2100),ylim=c(0,0.0025))

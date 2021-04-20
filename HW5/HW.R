f6fc0674184f87bb09e6674ecd95428fc25802e0
suphanut wandee <77426667+Filmsuphanut@users.noreply.github.com>
2021-04-20 16:35:51 +0700

    แก้

--- HW5/HW.R
+++ HW5/HW.R
@@ -1,14 +1,19 @@
 data <- read.csv("Data.csv")
 
-data.CO <- sort(data$CO_Emissions)
-data.Fuel <- sort(data$Fuel_Cost_6000_Miles)
+data.CO <- (data$CO_Emissions)
+data.Fuel <- (data$Fuel_Cost_6000_Miles)
 
 data.CO
 data.Fuel
 
 plot(data.Fuel,data.CO,main = "XY Scatter of CO_Emissions(mg/km) and FuelCost in 9600 km(Pound)",xlab = "FuelCost in 9600 km(Pound)",ylab = "CO_Emissions(mg/km)")
 
+cc <- cor(data.Fuel,data.CO)
+cd <- cc*cc
 LRmodel <- lm(data.CO ~ data.Fuel)
+cc
+cd
+LRmodel
 plot(data.Fuel,data.CO,main = "Linear Regression of CO_Emissions(mg/km) and FuelCost in 9600 km(Pound)",xlab = "FuelCost in 9600 km(Pound)",ylab = "CO_Emissions(mg/km)")
 abline(LRmodel)
 
